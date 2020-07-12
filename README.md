367436a410a24cd88aa9b4349d9b49f269a5f488
Michael Paulukonis <mpauluko@travelers.com>
2020-07-12 14:04:23 -0400

    Added working grid of colors on a loop - stubbed out color getter, no text

--- README.md
+++ README.md
@@ -7,6 +7,16 @@
 - "rebuild" the gif (a-la-imagetexter) with the text, frame by frame
 - Doesn't have to be saveable - just displayable
 
+- instead of a gif, could just use any image, and do a drunkard's walk through it
+- display a blown-up section, then move one of 8 directions and re-do
+- it "animates" the colors  
+
+## quick plan
+1. stub out text-gen (single-char, whatever)
+2. stub out color getter - alternate or something
+3. build the darn squares
+4. put some text in 'em
+
 ## some source references
 
 - https://developers.giphy.com/dashboard/
@@ -17,3 +27,5 @@
 - https://www.npmjs.com/package/gif-transparency
 - RITA
 - my tumblr code ???
+- https://stackoverflow.com/questions/48234696/how-to-put-a-gif-with-canvas
+- https://creative-coding.decontextualize.com/video/
--- src/sketch/index.js
+++ src/sketch/index.js
@@ -1,33 +1,95 @@
 // import p5Gif from 'p5.gif/dist/p5gif.min.js';
 import p5Gif from 'p5.gif/dist/p5gif.js';
 
+let config = {
+  paused: false,
+  delay: 100,
+  initialSize: 5,
+  stepSize: 5,
+  maxSteps: 20,
+  step: function () { logger('step'); singlestep = true; },
+  type: 2,
+  dMin: 5,
+  dMax: 20
+};
+let m = 0;
 
-export default function sketch({ p5Instance, gifs }) {
+export default function sketch ({ p5Instance, gifs }) {
   let x, y, backgroundColor;
 
   const width = 500;
   const height = 500;
 
-  window.p5 = p5Instance
-  var gif = new p5Gif.Gif(gifs[0].url);
-
   p5Instance.setup = () => {
+    p5Instance.noStroke();
+
     p5Instance.createCanvas(width, height);
     backgroundColor = p5Instance.color(p5Instance.random(255), p5Instance.random(255), p5Instance.random(255));
 
-    x = p5Instance.random(width);
-    y = height / 2;
   };
 
   p5Instance.draw = () => {
-    p5Instance.background(backgroundColor);
-    p5Instance.fill(p5Instance.color(255, 0, 0));
-    p5Instance.ellipse(x, y, 100, 100);
-
-    x = (x + 1) % width;
+    if (!config.paused && (p5Instance.millis() - m > config.delay)) {
+      drawPix();
+      m = p5Instance.millis();
+    }
   };
 
   p5Instance.mousePressed = () => {
     backgroundColor = p5Instance.color(p5Instance.random(255), p5Instance.random(255), p5Instance.random(255));
   };
+
+  const setPixSize = (direction, pixSize, stepSize) => {
+    pixSize = (pixSize + (direction * stepSize));
+    if (pixSize < stepSize) pixSize = stepSize;
+    return pixSize;
+  }
+
+  // loop purely for manual monitoring
+  // for export [for, say, a gif], do it faster
+  const drawPix = () => {
+    const pixSize = 20;
+    pixelateImageUpperLeft(pixSize);
+  }
+
+  // there's an issue where the right-hand strip comes and goes
+  // it's an average problem. probably "correct"
+  // but I don't like how it looks in a sequence
+  const pixelateImageUpperLeft = (pxSize) => {
+    // TODO: work from center of image outward
+    // or optionally pick the center
+    for (var x = 0; x < width; x += pxSize) {
+      for (var y = 0; y < height; y += pxSize) {
+        p5Instance.fill(getColor(x, y, pxSize));
+        p5Instance.rect(x, y, pxSize, pxSize);
+      }
+    }
+  }
+
+  // average code based on http://stackoverflow.com/a/12408627/41153
+  // this is likely to fail if xLoc,yLoc is with pixSize of width,height
+  // but works for what I'm currently doing....
+  const getColor = (xLoc, yLoc, cellSize) => {
+    // if (yLoc < 0) { yLoc = 0 }
+    // if (xLoc < 0) { xLoc = 0 }
+    // let r = 0, b = 0, g = 0;
+    // const pixelCount = cellSize * cellSize
+
+    // // could be faster if we grab this all first and calculate?
+    // const allPixels = img.drawingContext.getImageData(xLoc, yLoc, cellSize, cellSize).data
+    // for (let i = 0; i < allPixels.length; i += 4) {
+    //   r += allPixels[i]
+    //   g += allPixels[i + 1]
+    //   b += allPixels[i + 2]
+    //   // skip alpha
+    // }
+
+    // const averageColor = color(r / pixelCount, g / pixelCount, b / pixelCount);
+    // return averageColor;
+
+    // temp random
+    return p5Instance.color(p5Instance.random(255), p5Instance.random(255), p5Instance.random(255));
+  }
+
+
 }
