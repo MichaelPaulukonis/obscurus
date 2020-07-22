835bfd74274ba1462f3c613e0d8b51da8fe74e2d
Michael Paulukonis <mpauluko@travelers.com>
2020-07-21 22:10:08 -0400

    Added tumblr-random to get random texts One is used, and not implemented to shuffle Some cleanup, including file movement and re-naming

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import P5 from 'p5'
 import sketch from './sketch'
-import TextManager from './TextManager'
+import randomPost from './sketch/tumblr-random.js'
+import TextManager from './sketch/TextManager'
 import corpus from './sketch/corpus.default'
+
+const randElem = arr => arr[Math.floor(Math.random() * arr.length)]
+const textManager = new TextManager()
+let newCorpus = []
 
 const theStuff = () => {
     const builder = (p5Instance) => {
-        const textManager = new TextManager()
-        sketch({ p5Instance, textManager, corpus })
+        textManager.randomPost = randomPost
+        sketch({ p5Instance, textManager, corpus: newCorpus })
     }
 
-    new P5(builder)
+    randomPost()
+      .then((texts) => {
+        newCorpus = corpus.concat(texts)
+      })
+      .finally((_) => {
+        new P5(builder) // eslint-disable-line no-new
+      })
+
 }
 
 theStuff()
